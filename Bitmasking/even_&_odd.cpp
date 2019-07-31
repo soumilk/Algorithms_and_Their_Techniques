90f79ec55ec89e9a1bb49f1c0b686895c76976f9
Soumil Khandelwal <soumilk.k@gmail.com>
2019-07-31 20:58:09 +0530

    Add bitwise and operation for even and odd

---
+++ Bitmasking/even_&_odd.cpp
@@ -0,0 +1,41 @@
+//
+// Created by soumil on 7/16/2019.
+//
+
+/*
+ * THIS IS THE METHOD TO CALCULATE THE EVEN AND ODD NUMBERS FAST.
+ * IN THE BINARY OF THE NUMBER, IF THE NUMBER IS ODD THEN THE LAST BIT IN THE BINARY REPRESENTATION
+   WOULD BE SET (i.e. 1) AND IF THE NUMBER IS EVEN THEN THE LAST BIT WOULD NOT BE SET (i.e. 0)
+ * BY USING THIS CONCEPT, WE ARE GONNA CALCULATE THE ODD AND EVEN NUMBERS.
+ * BY TAKING THE 'AND' WITH THE LAST BIT, WE CAN FIND OUT THAT IF THE NUMBER IS EVEN OR ODD.
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    a=12;
+    b=17;
+    if (a&1)    // Applying and operator
+    {
+        cout<<a<<"\t";
+        cout<<"Hello odd"<<endl;
+    }
+    else
+    {
+        cout<<a<<"\t";
+        cout<<"Hello even "<<endl;
+    }
+    if (b&1)
+    {
+        cout<<b<<"\t";
+        cout<<"Hello odd "<<endl;
+    }
+    else
+    {
+        cout<<b<<"\t";
+        cout<<"Hello even "<<endl;
+    }
+    return 0;
+}
