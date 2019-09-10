973c178fea1f29c1fbe1466f1fc74827cd174f95
Soumil Khandelwal <soumilk.k@gmail.com>
2019-09-10 17:49:33 +0530

    Add python3 program to calculate expoents

---
+++ Recursion and Backtrack/fast power.py
@@ -0,0 +1,19 @@
+# This is the program to calculate the exponents much faster in O(log n base 2) time
+
+def power(num,expo):
+    ''' BASE CASE '''
+    if expo==0:
+        return 1
+
+    ''' RECURSSIVE CASE '''
+    ans=power(num,expo//2)
+    ans=ans*ans
+
+    ''' IF THE POWER IS ODD THEN MULTIPLY NUMBER ONE MORE TIME ''' 
+    if (expo&1):
+        ans=ans*num
+    return ans
+
+num,expo=map(int,input("Enter the number and the exponent\n").split())
+ans=power(num,expo)
+print(ans)
